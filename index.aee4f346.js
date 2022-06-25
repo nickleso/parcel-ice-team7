0b801b903fff8b249ed91c6e2b0e423631b3a814
nickleso <103372501+nickleso@users.noreply.github.com>
2022-06-25 14:42:49 +0000

    Deploying to gh-pages from @ nickleso/parcel-ice-team7@65b32e2b1c96c2945faec96fdc505f7ebe1713c5 🚀

--- index.aee4f346.js
+++ index.aee4f346.js
@@ -1,2 +1,2 @@
 document.querySelector(".desk__img-coffee").onclick=function(){document.querySelector(".products__text-coffee").classList.toggle("open")};
-//# sourceMappingURL=index.c3fe3040.js.map
+//# sourceMappingURL=index.aee4f346.js.map
--- index.b9e2aef5.js
+++ index.b9e2aef5.js
@@ -1,2 +1,2 @@
 document.querySelector(".desk__img-cream").onclick=function(){document.querySelector(".products__text-cream").classList.toggle("open")};
-//# sourceMappingURL=index.7a849de4.js.map
+//# sourceMappingURL=index.b9e2aef5.js.map
