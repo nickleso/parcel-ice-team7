da4d5e402adc87714d8c4d1ad3d4abd466fe194c
Nick Leso <nickleso.work@gmail.com>
2022-06-28 14:30:42 +0300

    Change scrits locations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// test script
 import './js/js-test.js';
 
+// header-hero scripts
 import './js/mobile-menu.js';
+import './js/read-more-hero.js';
+import './js/scroll.js';
+
+// products scripts
+import './js/read-more-cream.js';
+import './js/read-more-coffee.js';
+import './js/read-more-milk.js';
+import './js/animation.js';
